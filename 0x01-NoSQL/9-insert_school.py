9b21f3e993ea7548eedb8db9149cae01bad41f9c
elyton250 <nelysemathieu@gmail.com>
2024-05-14 16:34:22 +0200

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this adds accpoording to kwargs"""
+from pymongo import mongo_client
+
+
+def insert_school(mongo_collection, **kwargs):
+     """this add in in a collectiom"""
+     if kwargs:
+         result = mongo_collection.insert_one(kwargs)
+         return result._id
