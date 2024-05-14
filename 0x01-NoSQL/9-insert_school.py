154ef119e09ffa0569f200f970d50ef6b7f18475
elyton250 <nelysemathieu@gmail.com>
2024-05-14 16:46:29 +0200

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,7 +4,7 @@ from pymongo import mongo_client
 
 
 def insert_school(mongo_collection, **kwargs):
-     """this add in in a collectiom"""
-     if kwargs:
-         result = mongo_collection.insert_one(kwargs)
-         return result._id
+    """this add in in a collectiom"""
+    if kwargs:
+        result = mongo_collection.insert_one(kwargs)
+        return result._id
